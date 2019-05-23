441af48c2572d41226bda8ecb86e1a92fe8ca70d
artem popov <artemap98@gmail.com>
2019-05-23 22:40:49 +0700

    Added InitMatrixTest

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -5,16 +5,29 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    void MainClassCreationTest() {
+    public void MainClassCreationTest() {
         Game game = new Game();
         Assert.assertNotNull(game);
     }
 
     @Test
-    void CreateMatrixTest()
+    public void CreateMatrixTest()
     {
         Game game = new Game();
         Assert.assertNotNull(game.CreateMatrix());
+    }
+
+    @Test
+    public void InitMatrixText()
+    {
+        Game game = new Game();
+        int[][] matrix = game.InitMatrix(game.CreateMatrix());
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[i].length; j++)
+                Assert.assertEquals(0,matrix[i][j]);
+        }
+        
     }
 
 
