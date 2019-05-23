c7da6d5910ab53a53fa66aa8fbea5c09eb48ea25
artem popov <artemap98@gmail.com>
2019-05-23 22:25:43 +0700

    Added CreateMatrix, create matrix test successful

---
+++ src/main/java/Game.java
@@ -0,0 +1,13 @@
+public class Game {
+
+    public static void main(String[] args) {
+        System.out.println("512 game tdd");
+    }
+
+    public int[][] CreateMatrix()
+    {
+        int[][] matrix= new int[4][4];
+        return matrix;
+    }
+
+}
