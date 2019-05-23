3ce5888a2162321aed21a014034a1aabee948f61
artem popov <artemap98@gmail.com>
2019-05-23 23:03:36 +0700

    Created Main.java and main()

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,5 @@
 public class Game {
 
-    public static void main(String[] args) {
-        System.out.println("512 game tdd");
-    }
-
     public int[][] CreateMatrix()
     {
         int[][] matrix= new int[4][4];
@@ -14,7 +10,7 @@ public class Game {
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[i].length; j++)
                 matrix[i][j] = 0;
-            
+
         return matrix;
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("512 game tdd");
+        Game game = new Game();
+        int[][] gameMatrix = game.InitMatrix(game.CreateMatrix());
+        for (int i = 0; i < gameMatrix.length; i++) {
+            for (int j = 0; j < gameMatrix[i].length; j++) {
+                gameMatrix[i][j] = 0;
+                System.out.printf("%4d", gameMatrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
