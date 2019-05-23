5480c8b42038d2b85abb0a80a5012f9a2d2a9d45
artem popov <artemap98@gmail.com>
2019-05-23 21:09:05 +0700

    Added Game class. Game creation test successful

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class GameTest {
+
+    @Test
+    void MainClassCreationTest() {
+        Game game = new Game();
+        Assert.assertNotNull(game);
+    }
+
+    @Test
+    void MatrixCreationTest()
+    {
+
+    }
+
+
+
+};
+
